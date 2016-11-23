e784e527f83fd5cd77844f9e4ea6169db22deb54
Umar Mukhtar <umar@tuffah.info>
2016-11-24 01:06:08 +0800

    beta 1.0

--- www/assets/js/check_url.js
+++ www/assets/js/check_url.js
@@ -18,6 +18,12 @@
 function pass_url(page) {
     var adopt = $("#adopted3");
     
+    var pe = page.split("/");
+    if (pe[0] == 'member') {
+        change_title('member/title.html');
+        change_menubar('member/menubar.html');
+    }
+    
     adopt.attr("l", page);
     
     adopt.hide(0).load(page, function () {
